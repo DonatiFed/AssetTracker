25f554acaf776b246607490de105300dbed38026
federicodonati <Donati821@gmail.com>
2025-02-28 15:02:02 +0100

    import bug fixed

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
 import '../style.css';
-import {useEffect} from 'react';
 
 useEffect(() => {
     localStorage.clear();
